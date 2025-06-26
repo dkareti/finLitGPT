400ae072dc0fd8e9e23dfcd3a6667e2037775700
Daniel Kareti <danielkareti@gmail.com>
2025-06-26 14:18:59 -0700

    Modified the pre-processing code to target the main content

--- scrape_and_prepare.py
+++ scrape_and_prepare.py
@@ -14,11 +14,20 @@ urls = [
 
 def extract_text_func(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, 'html.parser') #this gets the raw bytes of the html of the website
 
     #retrive all the paragraphs in the main content section
     paragraphs = soup.find_all('p')
-    text = "\n".join([p.get_text() for p in paragraphs])
+
+    #specific for investopedia articles
+    '''
+    This narrows the text to the main content. Investopedia articles house 
+    the main content within the base class 'mntl-sc-block'
+    '''
+    filtered = [p for p in paragraphs if 'mntl-sc-block' in p.get('class', [])]
+
+    #this joins the clean text into arrays of strings.
+    text = "\n".join([p.get_text() for p in filtered])
     return text
 
 def chunk_text(text, max_words = 100):
