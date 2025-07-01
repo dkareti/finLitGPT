05da8e635aeb035e6f81d4ab7bbdfe4361ecb9f2
Daniel Kareti <danielkareti@gmail.com>
2025-07-01 12:53:33 -0700

    Finished all the code

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
 from flask import Flask, request, render_template, jsonify
+from rag_pipeline import get_answer
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    data = request.json
+    query = data.get('question')
+
+    if not query:
+        return jsonify({'error': 'No question provided'}), 400
+    answer = get_answer(query)
+    return jsonify({'answer': answer})
+
+if __name__ == '__main__':
+    app.run(debug=True)
